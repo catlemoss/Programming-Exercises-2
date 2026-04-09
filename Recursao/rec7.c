c36d4b8a0272f6d9d2ae12138acbb79248f73ccc
catlemoss <catsilveira2004@gmail.com>
2026-04-09 18:21:26 -0300

    rec7

---
+++ Recursao/rec7.c
@@ -0,0 +1,24 @@
+/*
+Um programa que recebe um número e retorna a soma dos seus dígitos, com recursão.
+Catarina Lemos
+09/04/2026
+*/
+
+#include <stdio.h>
+
+int somaDigito (int n)
+{
+    if (n == 0) return 0;
+
+    return n % 10 + somaDigito(n / 10); // pega com o %10 e remove com o /10
+}
+
+int main ()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", somaDigito(n));
+
+    return 0;
+}
